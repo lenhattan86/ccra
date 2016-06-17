66c1076d21c407bda01fea41cb732a2b922bd496
Tan N. Le <lenhattan86@gmail.com>
2016-06-17 10:46:33 -0400

    update

--- script/setup_flink_on_yarn.sh
+++ script/setup_flink_on_yarn.sh
@@ -4,10 +4,11 @@
 
 ## NOTES
 # 1. We have to install Ganglia web server manually
-#ssh tanle@$masterNode 'sudo apt-get install -y rrdtool  ganglia-webfrontend'
+#ssh $username@$masterNode 'sudo apt-get install -y rrdtool  ganglia-webfrontend'
 
 isCloudLab=true
 isAmazonEC=false
+username="tanle"
 
 java_home='/usr/lib/jvm/java-8-openjdk-amd64'
 
@@ -19,7 +20,7 @@ flinkTar="flink.tar"
 flinkVer="flink-1.0.3"
 flinkTgz="flink-1.0.3-bin-hadoop27-scala_2.10.tgz"
 flinkDownloadLink="http://apache.mesi.com.ar/flink/flink-1.0.3/flink-1.0.3-bin-hadoop27-scala_2.10.tgz"
-flinkSrc="/home/tanle/projects/Flink"
+flinkSrc="/home/$username/projects/Flink"
 testCase="../flink-test-cases"
 
 ###########
@@ -44,15 +45,16 @@ hdfsDir="/dev/hdfs"
 yarnAppLogs="/dev/shm/yarn-logs"
 #hdfsDir="/users/tanle/hdfs"
 #hdfsDir="/proj/yarnrm-PG0/hdfs"
+cgroupYarn="~/$hadoopVer/cgroup"
 numOfReplication=3
 numNetworkBuffers=4096 # default 2048
 ##########
 
-#log-dirs are bad: /users/tanle/hadoop-2.7.0/logs/userlogs
+#log-dirs are bad: /users/$username/hadoop-2.7.0/logs/userlogs
 
 
 REBOOT=false
-isOfficial=true
+isOfficial=false
 isSingleNodeCluster=false;
 
 isUploadTestCase=false
@@ -107,7 +109,7 @@ then
 	masterNode="nm"
 	clientNode="ctl"
 
-	privateKey="/home/tanle/Dropbox/Papers/System/Flink/cloudlab/cloudlab.pem"
+	privateKey="/home/$username/Dropbox/Papers/System/Flink/cloudlab/cloudlab.pem"
 	if $isOfficial
 	then
 		numOfworkers=8
@@ -150,7 +152,7 @@ echo ############### REBOOT all servers #############################
 
 	for server in $serverList; do		
 		echo reboot server $server
-		ssh tanle@$server "ssh $server 'sudo reboot'" &
+		ssh $username@$server "ssh $server 'sudo reboot'" &
 	done
 	wait
 fi
@@ -174,20 +176,20 @@ echo ################################# passwordless SSH ########################
 		echo upload keys to $server
 		if ! $isSingleNodeCluster
 		then
-			ssh tanle@$server 'sudo rm -rf $HOME/.ssh/id_dsa*'
-			scp ~/.ssh/id_dsa* tanle@$server:~/.ssh/
+			ssh $username@$server 'sudo rm -rf $HOME/.ssh/id_dsa*'
+			scp ~/.ssh/id_dsa* $username@$server:~/.ssh/
 		fi
-		ssh tanle@$server "cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys ;
+		ssh $username@$server "cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys ;
 		 chmod 0600 ~/.ssh/id_dsa*; 
 		 chmod 0600 ~/.ssh/authorized_keys; 
 		 rm -rf ~/.ssh/known_hosts; 	
 		 echo 'StrictHostKeyChecking no' >> ~/.ssh/config"
-#		ssh tanle@$server "echo password less from localhost to $server"
+#		ssh $username@$server "echo password less from localhost to $server"
 	done	
 fi
 if $isPasswordlessSSH
 then
-	passwordlessSSH () { echo $1 to $2;	ssh tanle@$1 "ssh $2 'echo test passwordless SSH'" ;}
+	passwordlessSSH () { echo $1 to $2;	ssh $username@$1 "ssh $2 'echo test passwordless SSH'" ;}
 	for server1 in $serverList; do
 		for server2 in $serverList; do
 			passwordlessSSH $server1 $server2 &
@@ -199,16 +201,18 @@ fi
 if $isInstallBasePackages
 then
 	echo ################################# install JAVA ######################################
-	installJava () {
-		ssh tanle@$1 'yes Y | sudo apt-get install openjdk-8-jdk'
+	installPackages () {
+		ssh $username@$1 'yes Y | sudo apt-get install openjdk-8-jdk'
+		ssh $username@$1 "sudo apt-get install -y cgroup-tools"	
 	}
 	echo "TODO: install JAVA"
 	for server in $serverList; do
-		installJava $server &
+		installPackages $server &
 	done
 	wait
 	echo ################################ install screen #####################################
-	ssh tanle@$masterNode "sudo apt-get install -y screen"
+	ssh $username@$masterNode "sudo apt-get install -y screen"
+	
 fi
 
 
@@ -216,14 +220,14 @@ if $isInstallGanglia
 then
 echo ################################# install Ganglia ###################################
 	echo "Configure Ganglia master node $masterNode"
-	ssh tanle@$masterNode 'yes Y | sudo apt-get purge ganglia-monitor gmetad'
+	ssh $username@$masterNode 'yes Y | sudo apt-get purge ganglia-monitor gmetad'
 	### PLZ manually install Ganglia as we need to respond to some pop-ups
 	# we may restart the Apache2 twice
-	#ssh tanle@$masterNode 'sudo apt-get install -y rrdtool  ganglia-webfrontend'
-	ssh tanle@$masterNode 'sudo apt-get install -y ganglia-monitor gmetad'
+	#ssh $username@$masterNode 'sudo apt-get install -y rrdtool  ganglia-webfrontend'
+	ssh $username@$masterNode 'sudo apt-get install -y ganglia-monitor gmetad'
 	
 	# 
-	ssh tanle@$masterNode "sudo cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf ;
+	ssh $username@$masterNode "sudo cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf ;
 	sudo sed -i -e 's/data_source \"my cluster\" localhost/data_source \"sbu flink\" 1 localhost/g' /etc/ganglia/gmetad.conf;
 	sudo sed -i -e 's/name = \"unspecified\"/name = \"sbu flink\"/g' /etc/ganglia/gmond.conf ;
 	sudo sed -i -e 's/mcast_join = 239.2.11.71/#mcast_join = 239.2.11.71/g' /etc/ganglia/gmond.conf;
@@ -231,7 +235,7 @@ echo ################################# install Ganglia #########################
 	sudo sed -i -e 's/udp_send_channel {/udp_send_channel { host=nm/g' /etc/ganglia/gmond.conf"
 	
 	installGangliaFunc(){
-		ssh tanle@$1 "yes Y | sudo apt-get purge ganglia-monitor;
+		ssh $username@$1 "yes Y | sudo apt-get purge ganglia-monitor;
 		sudo apt-get install -y ganglia-monitor;
 		sudo sed -i -e 's/name = \"unspecified\"/name = \"sbu flink\"/g' /etc/ganglia/gmond.conf;
 		sudo sed -i -e 's/mcast_join = 239.2.11.71/#mcast_join = 239.2.11.71/g' /etc/ganglia/gmond.conf;
@@ -249,9 +253,9 @@ if $startGanglia
 then
 	echo restart Ganglia
 	# restart all related services
-	ssh tanle@$masterNode 'sudo service ganglia-monitor restart & sudo service gmetad restart & sudo service apache2 restart'
+	ssh $username@$masterNode 'sudo service ganglia-monitor restart & sudo service gmetad restart & sudo service apache2 restart'
 	for server in $slaveNodes; do
-		ssh tanle@$server 'sudo service ganglia-monitor restart' &
+		ssh $username@$server 'sudo service ganglia-monitor restart' &
 	done
 	wait	
 fi
@@ -260,10 +264,10 @@ fi
 if $isShutDownHadoop
 then
 	echo shutdown Hadoop and Yarn
-	ssh tanle@$masterNode "$hadoopVer/sbin/stop-dfs.sh;
+	ssh $username@$masterNode "$hadoopVer/sbin/stop-dfs.sh;
 	$hadoopVer/sbin/stop-yarn.sh"
 #	$hadoopVer/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR stop proxyserver"
-#	ssh tanle@$masterNode "$hadoopVer/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR stop historyserver"
+#	ssh $username@$masterNode "$hadoopVer/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR stop historyserver"
 fi 
 if $isInstallHadoop
 then
@@ -279,24 +283,24 @@ echo "#################################### install Hadoop Yarn #################
 			if $isDownload
 			then		
 				echo downloading $hadoopVer		
-				ssh tanle@$1 "rm -rf $hadoopTgz; wget $hadoopLink"
+				ssh $username@$1 "rm -rf $hadoopTgz; wget $hadoopLink"
 			fi
 			if $isExtract
 			then 
 				echo extract $hadoopTgz
-				ssh tanle@$1 "rm -rf $hadoopVer; tar -xvzf $hadoopTgz >> log.txt"	
+				ssh $username@$1 "rm -rf $hadoopVer; tar -xvzf $hadoopTgz >> log.txt"	
 			fi
 			# add JAVA_HOME
 			echo Configure Hadoop at $1
-			ssh tanle@$1 "echo export JAVA_HOME=$java_home > temp.txt"			
-			ssh tanle@$1 "cat temp.txt ~/$hadoopVer/etc/hadoop/hadoop-env.sh > temp2.txt ; mv temp2.txt ~/$hadoopVer/etc/hadoop/hadoop-env.sh"
+			ssh $username@$1 "echo export JAVA_HOME=$java_home > temp.txt"			
+			ssh $username@$1 "cat temp.txt ~/$hadoopVer/etc/hadoop/hadoop-env.sh > temp2.txt ; mv temp2.txt ~/$hadoopVer/etc/hadoop/hadoop-env.sh"
 
 			if $isInitPath
 			then	
-				ssh tanle@$1 "echo export JAVA_HOME=$java_home >> .bashrc"				
+				ssh $username@$1 "echo export JAVA_HOME=$java_home >> .bashrc"				
 				# Administrators can configure individual daemons using the configuration options shown below in the table:	
-				#ssh tanle@$1 'echo export HADOOP_NAMENODE_OPTS="-XX:+UseParallelGC" > temp.txt'
-				#ssh tanle@$1 "cat /$hadoopVer/etc/hadoop/hadoop-env.sh temp.txt > temp2.txt; mv temp2.txt /$hadoopVer/etc/hadoop/hadoop-env.sh"
+				#ssh $username@$1 'echo export HADOOP_NAMENODE_OPTS="-XX:+UseParallelGC" > temp.txt'
+				#ssh $username@$1 "cat /$hadoopVer/etc/hadoop/hadoop-env.sh temp.txt > temp2.txt; mv temp2.txt /$hadoopVer/etc/hadoop/hadoop-env.sh"
 				# HADOOP_DATANODE_OPTS
 	 			# HADOOP_DATANODE_OPTS
 				# HADOOP_SECONDARYNAMENODE_OPTS	
@@ -309,12 +313,12 @@ echo "#################################### install Hadoop Yarn #################
 				# HADOOP_PID_DIR - The directory where the daemons’ process id files are stored.
 				# HADOOP_LOG_DIR - The directory where the daemons’ log files are stored. 
 				# HADOOP_HEAPSIZE
-				# ssh tanle@$1 "sed -i -e 's/#export HADOOP_HEAPSIZE=/export HADOOP_HEAPSIZE=4096/g' $hadoopVer/etc/hadoop/hadoop-env.sh"
+				# ssh $username@$1 "sed -i -e 's/#export HADOOP_HEAPSIZE=/export HADOOP_HEAPSIZE=4096/g' $hadoopVer/etc/hadoop/hadoop-env.sh"
 				# YARN_HEAPSIZE
-				# ssh tanle@$server "sed -i -e 's/# YARN_HEAPSIZE=1000/# YARN_HEAPSIZE=4096/g' $hadoopVer/etc/hadoop/yarn-env.sh"
+				# ssh $username@$server "sed -i -e 's/# YARN_HEAPSIZE=1000/# YARN_HEAPSIZE=4096/g' $hadoopVer/etc/hadoop/yarn-env.sh"
 			
 				# configure HADOOP_PREFIX 
-				ssh tanle@$1 "echo export HADOOP_PREFIX=~/$hadoopVer >> .bashrc;
+				ssh $username@$1 "echo export HADOOP_PREFIX=~/$hadoopVer >> .bashrc;
 				echo export HADOOP_YARN_HOME=~/$hadoopVer >> .bashrc;
 				echo export HADOOP_HOME=~/$hadoopVer >> .bashrc;				
 				echo export HADOOP_CONF_DIR=~/$hadoopVer/etc/hadoop >> .bashrc;
@@ -322,9 +326,9 @@ echo "#################################### install Hadoop Yarn #################
 			fi
 
 			# etc/hadoop/core-site.xml
-			ssh tanle@$1 "sudo rm -rf $hdfsDir; sudo mkdir $hdfsDir; sudo chmod 777 $hdfsDir"
+			ssh $username@$1 "sudo rm -rf $hdfsDir; sudo mkdir $hdfsDir; sudo chmod 777 $hdfsDir"
 
-			ssh tanle@$1 "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+			ssh $username@$1 "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
 <configuration>
 
@@ -346,7 +350,7 @@ echo "#################################### install Hadoop Yarn #################
 </configuration>' > $hadoopVer/etc/hadoop/core-site.xml"
 
 			# etc/hadoop/hdfs-site.xml
-			ssh tanle@$1 "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?> 
+			ssh $username@$1 "echo '<?xml version=\"1.0\" encoding=\"UTF-8\"?> 
 <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
 <configuration>
 
@@ -372,9 +376,9 @@ echo "#################################### install Hadoop Yarn #################
 			# etc/hadoop/yarn-site.xml
 			## Configurations for ResourceManager and NodeManager:
 
-			ssh tanle@$1 "sudo rm -rf $yarnAppLogs; sudo mkdir $yarnAppLogs; sudo chmod 777 $yarnAppLogs"			
+			ssh $username@$1 "sudo rm -rf $yarnAppLogs; sudo mkdir $yarnAppLogs; sudo chmod 777 $yarnAppLogs"			
 
-			ssh tanle@$1 "echo '<?xml version=\"1.0\"?>
+			ssh $username@$1 "echo '<?xml version=\"1.0\"?>
 <configuration>
   <property>
     <name>yarn.nodemanager.aux-services</name>
@@ -439,7 +443,8 @@ echo "#################################### install Hadoop Yarn #################
   </property>
 	
 <!-- CGroups -->
-	
+
+
 <!--
 	<property>
 		 <name>yarn.nodemanager.container-executor.class</name>
@@ -449,9 +454,15 @@ echo "#################################### install Hadoop Yarn #################
 		<name>yarn.nodemanager.linux-container-executor.resources-handler.class</name>
 		<value>org.apache.hadoop.yarn.server.nodemanager.util.CgroupsLCEResourcesHandler</value>
 	</property>
+
+	<property>
+		<name>yarn.nodemanager.linux-container-executor.group</name>
+		<value>root</value>
+	</property> 
+
 	<property>
 		<name>yarn.nodemanager.linux-container-executor.cgroups.mount</name>
-		<value>true</value>
+		<value>false</value>
 	</property>
 	<property>
 		<name>yarn.nodemanager.linux-container-executor.cgroups.mount-path</name>
@@ -461,10 +472,7 @@ echo "#################################### install Hadoop Yarn #################
 		<name>yarn.nodemanager.linux-container-executor.cgroups.hierarchy</name>
 		<value>/hadoop-yarn</value> 		
 	</property>
-	<property>
-		<name>yarn.nodemanager.linux-container-executor.group</name>
-		<value>root</value>
-	</property> 
+	
 	
 	<property>
 		<name>yarn.nodemanager.linux-container-executor.nonsecure-mode.limit-users</name>
@@ -472,7 +480,7 @@ echo "#################################### install Hadoop Yarn #################
 	</property> 	
 	<property>
 		<name>yarn.nodemanager.linux-container-executor.nonsecure-mode.local-user</name>
-		<value>tanle</value>
+		<value>$username</value>
 	</property> 
 -->
 
@@ -480,21 +488,23 @@ echo "#################################### install Hadoop Yarn #################
 
 #yarn.nodemanager.linux-container-executor.group=#configured value of yarn.nodemanager.linux-container-executor.group
 #allowed.system.users=##comma separated list of system users who CAN run applications
-			ssh tanle@$1 "sudo sed -i -e 's/yarn.nodemanager.linux-container-executor.group=#/yarn.nodemanager.linux-container-executor.group=tanle#/g' $hadoopVer/etc/hadoop/container-executor.cfg"
+			ssh $username@$1 "sudo sed -i -e 's/yarn.nodemanager.linux-container-executor.group=#/yarn.nodemanager.linux-container-executor.group=root#/g' $hadoopVer/etc/hadoop/container-executor.cfg"
 
-#			ssh tanle@$1 "sudo chmod -R 777 /sys/fs/cgroup; sudo mkdir /sys/fs/cgroup/cpu/yarn; sudo chmod -R 777 /sys/fs/cgroup/cpu/yarn"	
+			ssh $username@$1 "sudo mkdir $cgroupYarn"
+#			ssh $username@$1 "sudo chmod -R 777 $cgroupYarn"			
+#			ssh $username@$1 "cgdelete cpu:yarn"
 
 # setup scheduler https://hadoop.apache.org/docs/r2.7.1/hadoop-yarn/hadoop-yarn-site/FairScheduler.html
-			ssh tanle@$1 "echo '<?xml version=\"1.0\"?>
+			ssh $username@$1 "echo '<?xml version=\"1.0\"?>
 <allocations>
   <queue name=\"sls_queue_1\">
     <minResources>1024 mb, 1 vcores</minResources>
-    <schedulingPolicy>fair</schedulingPolicy>
+    <schedulingPolicy>drf</schedulingPolicy>
     <weight>1</weight>
   </queue>
   <queue name=\"sls_queue_2\">
     <minResources>1024 mb, 1 vcores</minResources>
-    <schedulingPolicy>fair</schedulingPolicy>
+    <schedulingPolicy>drf</schedulingPolicy>
     <weight>1</weight>
   </queue>
   <queue name=\"sls_queue_3\">
@@ -507,19 +517,9 @@ echo "#################################### install Hadoop Yarn #################
     <schedulingPolicy>fair</schedulingPolicy>
     <weight>1</weight>
   </queue>	
-  <queue name=\"sls_queue_5\">
-    <minResources>1024 mb, 1 vcores</minResources>
-    <schedulingPolicy>fair</schedulingPolicy>
-    <weight>1</weight>
-  </queue>	
-  <queue name=\"sls_queue_6\">
-    <minResources>1024 mb, 1 vcores</minResources>
-    <schedulingPolicy>fair</schedulingPolicy>
-    <weight>1</weight>
-  </queue>	
 </allocations>' > $fairSchedulerFile"
 
-			ssh tanle@$1 "echo '<?xml version=\"1.0\"?>
+			ssh $username@$1 "echo '<?xml version=\"1.0\"?>
 <configuration>
   <property>
     <name>yarn.scheduler.capacity.resource-calculator</name>
@@ -563,7 +563,7 @@ echo "#################################### install Hadoop Yarn #################
 </configuration>' > $capacitySchedulerFile"
 		
 			# etc/hadoop/mapred-site.xml
-			ssh tanle@$1 "echo '<?xml version=\"1.0\"?>
+			ssh $username@$1 "echo '<?xml version=\"1.0\"?>
 <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
 <configuration>
 
@@ -615,9 +615,9 @@ echo "#################################### install Hadoop Yarn #################
 			# monitoring script in etc/hadoop/yarn-site.xml
 
 			# slaves etc/hadoop/slaves
-			ssh tanle@$1 "rm -rf $hadoopVer/etc/hadoop/slaves"
+			ssh $username@$1 "rm -rf $hadoopVer/etc/hadoop/slaves"
 			for svr in $slaveNodes; do
-				ssh tanle@$1 "echo $svr >> $hadoopVer/etc/hadoop/slaves"
+				ssh $username@$1 "echo $svr >> $hadoopVer/etc/hadoop/slaves"
 			done	
 			
 }
@@ -635,20 +635,20 @@ if $restartHadoop
 then
 	# shutdown all before starting.
 	echo "============================stopping Hadoop (HDFS) and Yarn ========================="
-	ssh tanle@$masterNode "$hadoopVer/sbin/stop-dfs.sh; $hadoopVer/sbin/stop-yarn.sh"
+	ssh $username@$masterNode "$hadoopVer/sbin/stop-dfs.sh; $hadoopVer/sbin/stop-yarn.sh"
 	echo '============================ starting Hadoop==================================='
 
 	if $isFormatHDFS
 	then
-		ssh tanle@$masterNode "sudo rm -rf $hdfsDir; sudo mkdir $hdfsDir; sudo chmod 777 $hdfsDir"
-		ssh tanle@$masterNode "yes Y | $hadoopVer/bin/hdfs namenode -format HDFS4Flink"
+		ssh $username@$masterNode "sudo rm -rf $hdfsDir; sudo mkdir $hdfsDir; sudo chmod 777 $hdfsDir"
+		ssh $username@$masterNode "yes Y | $hadoopVer/bin/hdfs namenode -format HDFS4Flink"
 	fi
-	ssh tanle@$masterNode "$hadoopVer/sbin/start-dfs.sh"
+	ssh $username@$masterNode "$hadoopVer/sbin/start-dfs.sh"
 	echo '============================ starting Yarn==================================='
 	# operating YARN
-	ssh tanle@$masterNode "$hadoopVer/sbin/start-yarn.sh"
+	ssh $username@$masterNode "$hadoopVer/sbin/start-yarn.sh"
 	# operating MapReduce
-	#ssh tanle@$masterNode '$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver'
+	#ssh $username@$masterNode '$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver'
 
 	
 fi
@@ -714,9 +714,9 @@ then
 	cd $flinkSrc	
 	rm -rf test/wordcount/*.txt test/wordcount/*.out test/wordcount/*.log
 	tar zcvf test.tar $testCase
-	ssh tanle@$masterNode 'rm -rf test*'
-	scp test.tar tanle@$masterNode:~/ 
-	ssh tanle@$masterNode 'tar -xvzf test.tar'
+	ssh $username@$masterNode 'rm -rf test*'
+	scp test.tar $username@$masterNode:~/ 
+	ssh $username@$masterNode 'tar -xvzf test.tar'
 
 	rm -rf test.tar
 fi
